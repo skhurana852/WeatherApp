e24547d1af6d462ee95656568f13a9a692743777
skhurana852 <saranshkhurana5@gmail.com>
2019-08-10 14:51:13 +0530

    Added Images in Weather Description

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ state={
   country : undefined,
   humidity: undefined,
   description: undefined,
-  error: undefined
+  error: undefined,
+  ImageUrl:'https://media.giphy.com/media/l1J9w4UPRtjZtPsA0/giphy.gif'
 }
 
 getWeather =  async (e) =>{
+
 
   let city = document.getElementById("city").value;
 
@@ -22,10 +24,12 @@ getWeather =  async (e) =>{
 
   console.log(city);
   console.log(country);
+  //fethcing data using fetch library in an asynchronous function
   const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=a1d5dd29b06c7cbaf7523098e9726acc`)
   
+  //converting data into json
   const response = await api_call.json();
-  console.log(response);
+  //console.log(response);
 
   if(city&& country){
 
@@ -36,8 +40,10 @@ getWeather =  async (e) =>{
     country: response.sys.country,
     humidity: response.main.humidity,
     description: response.weather[0].description,
-    error: ""
+    error: "",
+    
   })
+  
    
 }
 
@@ -47,19 +53,44 @@ else{
   })
   alert("Please Fill Fields");
 }
+
+console.log(response.weather[0].main);
+
+if(response.weather[0].main==='Rain'){
+  this.setState({
+    ImageUrl: 'https://media.giphy.com/media/RIexS5Dd3x0pMEppVE/giphy.gif'
+  })
+}
+else if(response.weather[0].main==='Clouds'){
+  this.setState({
+    ImageUrl: 'https://media.giphy.com/media/3ohhwsupwJyzktdgS4/giphy.gif'
+  })
+}
+else if(response.weather[0].main==='Clear'){
+  this.setState({
+    ImageUrl: 'https://media.giphy.com/media/CvmdavnVFX0d2/giphy.gif'
+  })
+}
+else if(response.weather[0].main==='Haze'){
+  this.setState({
+    ImageUrl: 'https://media.giphy.com/media/cNwdp35dM1Ue4UUaDS/giphy.gif'
+  })
+}
 }
 render(){
   return (
     
-    <div className="App">
+    <div className="App" style={{backgroundImage:'weather.jpg'}}>
       <Title />
       <div id="form">
+         
       <form > 
-          <div className="input"> <input type="text" id="city" placeholder="Enter city"/></div>
-          <div className="input">  <input type="text" id="country" placeholder="Enter Country"/></div><br/><br/>
+      <div className="input">  <input type="text" id="city" placeholder="Enter City"/></div>
+          <div className="input"> <input type="text" id="country" placeholder="Enter County"/> </div><br/><br/>
             <input type="button" onClick={this.getWeather} value="Get Weather" id="submit"/>
 
         </form>
+
         </div>
 
       <Weather 
@@ -68,7 +99,8 @@ render(){
       country={this.state.country}
       humidity={this.state.humidity}
       description={this.state.description}
-      error={this.state.error} />
+      error={this.state.error}
+      ImageUrl={this.state.ImageUrl} />
 
       <footer id="footer">
         <small>&copy;Copyright Saransh Khurana 2019, All Rights Reserved </small>
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Weather.css';
-
 const Weather =(props) =>
 {
     return(
        <div id="weatherdiv">
             <div id="weatherdetails">
+            <img src={props.ImageUrl} alt="Weather"  className="image"/>
             {props.country && props.city && <p>Location: {props.city},    {props.country}</p>}
       {props.temperature && <p>Temperature: {Math.floor(props.temperature-273.15)}&#176;c</p>}
       {props.humidity && <p>Humidity: {props.humidity}%</p>}
